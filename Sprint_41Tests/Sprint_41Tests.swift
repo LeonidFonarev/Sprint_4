aa89f1ecad5a38c94d9db297351f67fd98c6cc2a
Фонарёв Леонид Александрович <45788353+LeonidFonarev@users.noreply.github.com>
2025-04-11 21:11:34 +0400

    Рега

---
+++ Sprint_41Tests/Sprint_41Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Sprint_41Tests.swift
+//  Sprint_41Tests
+//
+//  Created by Leonid on 11.04.2025.
+//
+
+import Testing
+@testable import Sprint_41
+
+struct Sprint_41Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
